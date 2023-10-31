f15fc58ea88a4ac140961c0bb75403cf8604ef4b
arnavr15 <arnav.ranjan@likeminds.community>
2023-10-31 12:18:39 +0530

    changed migration logic

--- src/localDb/db/realmDbMigration.ts
+++ src/localDb/db/realmDbMigration.ts
@@ -8,53 +8,9 @@ export const realmDbMigration = (oldVersion: Realm, newVersion: Realm) => {
     // Perform the migration by iterating through existing objects and setting the new property
     for (let i = 0; i < oldObjects.length; i++) {
       const oldObject = oldObjects[i];
-      newVersion.create(ConversationRO.schema.name, {
-        id: oldObject?.id,
-        chatroomId: oldObject?.chatroomId,
-        communityId: oldObject?.communityId,
-        cardId: oldObject?.cardId,
-        member: oldObject?.member,
-        ogTags: null, // Initialize the new property for existing objects
-        answer: oldObject?.answer,
-        state: oldObject?.state,
-        createdEpoch: oldObject?.createdEpoch,
-        createdAt: oldObject?.createdAt,
-        attachments: oldObject?.attachments,
-        date: oldObject?.date,
-        isEdited: oldObject?.isEdited,
-        lastSeen: oldObject?.lastSeen,
-        replyConversation: oldObject?.replyConversation,
-        replyConversationId: oldObject?.replyConversationId,
-        replyConversationObject: oldObject?.replyConversationObject,
-        deletedBy: oldObject?.deletedBy,
-        attachmentCount: oldObject?.attachmentCount,
-        userId: oldObject?.userId,
-        attachmentsUploaded: oldObject?.attachmentsUploaded,
-        uploadWorkerUUID: oldObject?.uploadWorkerUUID,
-        localSavedEpoch: oldObject?.localSavedEpoch,
-        temporaryId: oldObject?.temporaryId,
-        reactions: oldObject?.reactions,
-        isAnonymous: oldObject?.isAnonymous,
-        allowAddOption: oldObject?.allowAddOption,
-        pollType: oldObject?.pollType,
-        pollTypeText: oldObject?.pollTypeText,
-        submitTypeText: oldObject?.submitTypeText,
-        expiryTime: oldObject?.expiryTime,
-        multipleSelectNo: oldObject?.multipleSelectNo,
-        multipleSelectState: oldObject?.multipleSelectState,
-        polls: oldObject?.polls,
-        pollAnswerText: oldObject?.pollAnswerText,
-        toShowResults: oldObject?.toShowResults,
-        replyChatRoomId: oldObject?.replyChatRoomId,
-        replyId: oldObject?.replyId,
-        isInProgress: oldObject?.isInProgress,
-        hasFiles: oldObject?.hasFiles,
-        lastUpdatedAt: oldObject?.lastUpdatedAt,
-        deletedByMember: oldObject?.deletedByMember,
-        deletedByUserId: oldObject?.deletedByUserId,
-        community: oldObject?.community,
-        chatroom: oldObject?.chatroom,
-      });
+      // Initialize the new property for existing objects
+      const newObject = { ...oldObject, ogTags: null };
+      newVersion.create(ConversationRO.schema.name, newObject);
     }
   }
 };
