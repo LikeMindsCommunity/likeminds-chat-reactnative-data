4b810a889bf7f44739dff3b5af874ccd10a28b18
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-12-21 12:42:51 +0530

    added migration for filter state conversation

--- src/localDb/db/realmDbMigration.ts
+++ src/localDb/db/realmDbMigration.ts
@@ -1,4 +1,7 @@
+import { APP_CONFIG, FILTER_CONVERSATION_STATE_RO } from "../constants";
+import { AppConfigRO } from "../models/AppConfigRO";
 import { ConversationRO } from "../models/ConversationRO";
+import { FilterConversationStateRO } from "../models/FilterConversationStateRO";
 
 export const realmDbMigration = (oldVersion: Realm, newVersion: Realm) => {
   let oldSchemaVersion = oldVersion.schemaVersion;
@@ -6,16 +9,38 @@ export const realmDbMigration = (oldVersion: Realm, newVersion: Realm) => {
     const oldObjects = oldVersion.objects<ConversationRO>(
       ConversationRO.schema.name
     );
-    // Perform the migration by iterating through existing objects and setting the new property
+
     for (let i = 0; i < oldObjects.length; i++) {
       const oldObject = oldObjects[i];
       // Initialize the new property for existing objects
       const newObject = { ...oldObject, ogTags: null };
       newVersion.create(ConversationRO.schema.name, newObject);
     }
+
+    // Increment schema version
+    oldSchemaVersion++;
+  }
+  if (oldSchemaVersion == 2) {
+    const oldObjects = oldVersion.objects<AppConfigRO>(AppConfigRO.schema.name);
+
+    for (let i = 0; i < oldObjects.length; i++) {
+      const oldObject = oldObjects[i];
+      // Initialize the new property for existing objects
+      const newObject = { ...oldObject, id: APP_CONFIG };
+      newVersion.create(AppConfigRO.schema.name, newObject);
+    }
+
+    // Initialise new schema
+    newVersion.create(
+      FILTER_CONVERSATION_STATE_RO,
+      FilterConversationStateRO.schema.properties
+    );
+    oldVersion._updateSchema(newVersion.schema);
+
+    // Increment schema version
     oldSchemaVersion++;
   }
 };
 
 export const DB_SCHEMA_NAME = "likeminds-chat-sdk-rn";
-export const DB_SCHEMA_VERSION = 2;
+export const DB_SCHEMA_VERSION = 3;
