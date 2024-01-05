2e64ed92e350543c19f9c9050698f6c9c53e02ba
arnavr15 <arnav.ranjan@likeminds.community>
2024-01-05 16:20:07 +0530

    added cohort and channel support

--- src/index.ts
+++ src/index.ts
@@ -161,6 +161,7 @@ import { GetConversationNotificationUnreadResponse } from "./pages/chatroom/resp
 import { getUserSchema, setUserSchema } from "./localDb/db/queries/userSchema";
 import { setFilterConversationState } from "./localDb/db/queries/filterConversationState";
 import DLClient from "@likeminds.community/chat-js";
+import { AddMemberToCohort } from "./pages/user/responseModels/AddMemberToCohort";
 
 class LMChatClient {
   private static apiKey: string | null = null;
@@ -581,6 +582,16 @@ class LMChatClient {
     initUser: InitUserWithUuid
   ): Promise<LMResponse<InitiateUserResponse>> {
     return this.userClient.initiateUser(initUser, LMChatClient.dlClient);
+  }
+
+  // Method to add an user to a cohort
+  addMemberToCohort(
+    addMemberToCohort: AddMemberToCohort
+  ): Promise<LMResponse<Nothing>> {
+    return this.userClient.addMemberToCohort(
+      addMemberToCohort,
+      LMChatClient.dlClient
+    );
   }
 
   // Method to logout an user
