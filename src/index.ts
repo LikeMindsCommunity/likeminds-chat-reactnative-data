223352581d3222713c0209db427d23a2a0aa2e7c
arnavr15 <arnav.ranjan@likeminds.community>
2024-04-12 20:19:41 +0530

    added release related changes

--- src/index.ts
+++ src/index.ts
@@ -193,7 +193,7 @@ class LMChatClient {
     LMChatClient.dlClient = new DLClient({
       xApiKey: this.apiKey!,
       xPlatformCode: "rn",
-      xVersionCode: 30,
+      xVersionCode: 31,
       xSdkSource: "chat",
     });
 
